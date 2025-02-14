d62167acae02827bd73d91a8e46ebe789bc095ec
Serena1432 <admin@s1432.org>
2025-02-14 20:52:47 +0700

    Add file size definition function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const path = require("path");
 var modal = new Modal();
 
-var fnScript = "";
+var fnScript = "", fsScript = "";
 
 function codeEditor(element) {
     return CodeMirror.fromTextArea(element, {
@@ -27,38 +27,52 @@ function chooseFolder() {
     input.click();
 }
 
-function fileNameScript() {
-    modal.title = "File Name Script";
+function codeEditorModal(type = "Name") {
+    modal.title = `File ${type} Script`;
     modal.body = `
         <div class="grid script">
-            <div>You can write a script with function <code>defineName(index)</code> defining the file name for each file (index), with <code>index</code> variable being the current index number of that file (from 0 to length - 1).</div>
+            <div>You can write a script with function <code>define${type}(index)</code> defining the file name for each file (index), with <code>index</code> variable being the current index number of that file (from 0 to length - 1).</div>
             <div>
                 <textarea id="code"></textarea>
             </div>
         </div>
     `;
     modal.footer = `
-        <button type="button" class="btn btn-primary" onclick="saveFileNameScript()">Save</button>
+        <button type="button" class="btn btn-primary" onclick="saveFile${type}Script()">Save</button>
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
     `;
     modal.show();
-    if (!fnScript) $("#code").val(`function defineName(index) {\n\treturn index;\n}`);
+    if (!fnScript) $("#code").val(`function define${type}(index) {\n\treturn index;\n}`);
     else $("#code").val(fnScript);
     modal.editor = codeEditor(document.getElementById("code"));
     setTimeout(() => modal.editor.refresh(), 500);
 }
 
-function saveFileNameScript() {
-    var script = modal.editor.getValue();
+function testScript(script) {
+    script += "defineName(0)";
     try {
         // Test the script syntax
         (function() { eval(script) })();
     }
     catch (err) {
         alert(`Syntax error in define script. Please check the script again.\n${err.stack}`);
-        return;
+        return false;
     }
+    return true;
+}
+
+function saveFileNameScript() {
+    var script = modal.editor.getValue();
+    if (!testScript(script)) return;
     fnScript = script;
     modal.hide();
     $("#fileNameRadio3").prop("checked", true);
+}
+
+function saveFileSizeScript() {
+    var script = modal.editor.getValue();
+    if (!testScript(script)) return;
+    fsScript = script;
+    modal.hide();
+    $("#fileSizeRadio3").prop("checked", true);
 }
